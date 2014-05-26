98ffda53e79bd58599d099a9b749a73c6ad2a7a6
Damien Dube <damien.dube@gmail.com>
2014-05-26 11:23:43 -0400

    minor fixes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 rpm-uploader
 ============
+
+rpm-uploader is a small webserver that can take post 
+
+
+
+Start server
+rpm-uploader --output-dir /var/www/rpms [--port 1234 ] [ --dir release/2.1.3 ] [ --createrepo /usr/bin/createrepo ]
+
+
+curl -i -F data=@test_post.sh "http://${HOSTNAME}:1234/upload?project=care-reflex&branch=release/1.2.3"
--- rpm_uploader/__init__.py
+++ rpm_uploader/__init__.py
@@ -13,8 +13,7 @@ configs={}
 def do_upload():
     global configs
     data = request.files.data
-    project = request.query.get('project')
-    branch = request.query.get('branch')
+    project = request.query.get('dir')
     if not data or not data.file:
         response.status = 400
         return dict(message='Missing parameters')
@@ -31,19 +30,19 @@ def do_upload():
     data.save(file_path)
 
     # Update repo
-    subprocess.call([configs['create_repo'], '-p', '--update', output_dir])
+    subprocess.call([configs['createrepo'], '-p', '--update', output_dir])
 
     return dict(result="Success", message="Ok")
     
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output-dir', default='/var/www/install/rpms')
-    parser.add_argument('--create-repo', default='/usr/bin/createrepo')
+    parser.add_argument('--output-dir')
     parser.add_argument('--port', default='1234', type=int)
+    parser.add_argument('--createrepo', default='/usr/bin/createrepo')
     configs = vars(parser.parse_args(sys.argv[1:]))
 
     #Run server
-    print "Starting on port %s. Will save in %s and run %s in repo." % (configs['port'], configs['output_dir'], configs['create_repo'])
+    print "Starting on port %s. Will save in %s and run %s in repo." % (configs['port'], configs['output_dir'], configs['createrepo'])
     run(host=socket.gethostname(), port=configs['port'])
 
 if __name__ == '__main__':
